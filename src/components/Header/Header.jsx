55bb2864f8a5b2b299829bc9d3f585d4553b3c95
LarsWeyen <97236276+LarsWeyen@users.noreply.github.com>
2024-01-23 18:49:05 +0100

    feat: added a header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react'
+import './header.css'
+
+const Header = ({name,instruction, onNextClick,keys}) => {
+  const pressedKeys = [];
+  const [disabled, setDisabled] = useState(true);
+  
+  const handleKeyPress = (e) => {
+    if(keys.includes(e.key)){
+      pressedKeys.push(e.key);
+      const elementId = keys[keys.indexOf(e.key)];
+      document.getElementById(elementId).classList.add('active-key')     
+      if(pressedKeys.length === keys.length){
+        setDisabled(false);
+      }
+    }
+  }
+  useEffect(()=>{
+    console.log(keys)
+    document.addEventListener('keydown',handleKeyPress)
+  })
+  return (
+    <header>
+      <div className='header-container'>
+        <h1>{name}</h1>
+        <p dangerouslySetInnerHTML={{__html:instruction}}></p>
+        <button onClick={() => { onNextClick(); setDisabled(true); }} className='next-button' disabled={disabled}>Next</button>
+      </div>
+        
+    </header>
+  )
+}
+
+export default Header
