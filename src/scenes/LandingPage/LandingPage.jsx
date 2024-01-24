5fa6f32329a41b5eef8d028c9a661a45c54669ef
LarsWeyen <97236276+LarsWeyen@users.noreply.github.com>
2024-01-24 14:42:29 +0100

    feat: added landingpage content and styling

--- src/scenes/LandingPage/LandingPage.jsx
+++ src/scenes/LandingPage/LandingPage.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import './landingPage.css'
 
 const LandingPage = () => {
   return (
-    <div>LandingPage</div>
+    <div className='landing-container'>
+      <div className='landing-hero-container'>
+        <h1 className='hero-title'>Master Visual Studio <span>Code</span> Shortcuts</h1>
+        <p>Shortcuts Made Simple: Elevate Your Coding Game</p>
+        <Link to="/code">
+          <div className='hero-button'>
+              <span>Get Started</span>
+          </div>
+        </Link>
+      </div>
+      <div>
+
+      </div>
+    </div>
   )
 }
 
