37143e2886c4699b2b78fcba426e12e213b0c528
LarsWeyen <97236276+LarsWeyen@users.noreply.github.com>
2024-01-24 12:02:00 +0100

    feat: added a reset handler

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,14 +2,16 @@ import React, { useEffect, useState } from 'react'
 import './header.css'
 
 const Header = ({name,instruction, onNextClick,keys}) => {
-  const pressedKeys = [];
+  let pressedKeys = [];
   const [disabled, setDisabled] = useState(true);
   
   const handleKeyPress = (e) => {
+    
     if(keys.includes(e.key)){
       if(!pressedKeys.includes(e.key)){
         pressedKeys.push(e.key);
       }
+
       const elementId = keys[keys.indexOf(e.key)];
       document.getElementById(elementId).classList.add('active-key')     
       if(pressedKeys.length === keys.length){
@@ -17,6 +19,13 @@ const Header = ({name,instruction, onNextClick,keys}) => {
       }
     }
   }
+
+  const handleReset = () => {
+    pressedKeys.length = 0;
+    onNextClick();
+    setDisabled(true);
+  }
+
   useEffect(()=>{
     document.addEventListener('keydown',handleKeyPress)
   })
@@ -25,7 +34,7 @@ const Header = ({name,instruction, onNextClick,keys}) => {
       <div className='header-container'>
         <h1>{name}</h1>
         <p dangerouslySetInnerHTML={{__html:instruction}}></p>
-        <button onClick={() => { onNextClick(); setDisabled(true); }} className='next-button' disabled={disabled}>Next</button>
+        <button onClick={handleReset} className='next-button' disabled={disabled}>Next</button>
       </div>
         
     </header>
