797f54d33a0417aac8d0125297445bc7617af806
LarsWeyen <97236276+LarsWeyen@users.noreply.github.com>
2024-01-25 14:15:23 +0100

    refactor: changed order of shortcuts

--- src/data/shortcuts.js
+++ src/data/shortcuts.js
@@ -1,51 +1,51 @@
 var shortcuts =[
     {
-        name: "shortcut 1",
+        name: "Shortcut 1",
         instruction: "Press <code id='End'>End</code> or <code id='Home'>Home</code> to move the cursor to the beginning or end of the line.",
         function: "selectText",
         keys:["Home","End"]
     },
     {
-        name: "shortcut 2",
+        name: "Shortcut 2",
         instruction: "Put your cursor on a word and press <code id='Alt'>ALT</code> <code id='Shift'>Shift</code> <code id='ArrowRight'>--></code> to select that word",
         function: "selectText",
         keys:["Alt","Shift","ArrowRight"]
     },
     {
-        name: "shortcut 3",
+        name: "Shortcut 3",
         instruction: "While your cursor is in a line press <code id='Alt'>ALT</code> and <code id='ArrowUp'>ArrowUp</code> or <code id='ArrowDown'>ArrowUp</code> to move that line upwards or downwards",
         function: "moveLine",
         keys:["Alt","ArrowUp","ArrowDown"]
     },
     {
-        name: "shortcut 4",
-        instruction: "While your cursor is in a line press <code id='Shift'>Shift</code> <code id='Alt'>ALT</code> and <code id='ArrowUp'>ArrowUp</code> or <code id='ArrowDown'>ArrowUp</code> to copy that line",
-        function: "copyLine",
-        keys:["Shift","Alt","ArrowUp","ArrowDown"]
-    },
-    {
-        name: "shortcut 5",
+        name: "Shortcut 4",
         instruction: "While your cursor is in a line press <code id='Control'>Ctrl</code> <code id='Shift'>Shift</code> and <code id='K'>K</code> to delete that line",
         function: "deleteLine",
         keys:["Control","Shift","K"]
     },
     {
-        name: "shortcut 6",
+        name: "Shortcut 5",
         instruction: "While your cursor is in a line press <code id='Control'>Ctrl</code> <code id='['>[</code> or <code id=']'>]</code> to indent or outdent that line",
         function: "indentOrOutdentLine",
         keys:["Control","]","["]
     },
     {
-        name: "shortcut 7",
+        name: "Shortcut 6",
         instruction: "While your cursor is in a line press <code id='Control'>Ctrl</code> <code id='End'>End</code> or <code id='Home'>Home</code> to move to the beginning or end of the file",
         function: "endOrBeginningLine",
         keys:["Control","Home","End"]
     },
     {
-        name: "shortcut 8",
+        name: "Shortcut 7",
         instruction: "While your cursor is in a line press <code id='Control'>Ctrl</code> <code id='/'>/</code> to toggle line comment",
         function: "toggleLineComment",
         keys:["Control","/"]
+    },
+    {
+        name: "Shortcut 8",
+        instruction: "While your cursor is in a line press <code id='Shift'>Shift</code> <code id='Alt'>ALT</code> and <code id='ArrowUp'>ArrowUp</code> or <code id='ArrowDown'>ArrowUp</code> to copy that line",
+        function: "copyLine",
+        keys:["Shift","Alt","ArrowUp","ArrowDown"]
     }
 ]
 
